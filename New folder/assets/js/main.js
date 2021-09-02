1f5104370aa16cc86359c1e7b55bf7fd2f3d531d
Alisson G. Da Silva <giovanecontact@gmail.com>
2021-09-02 14:10:45 -0300

    NavBar + WallPaper + Quem Somos

---
+++ New folder/assets/js/main.js
@@ -0,0 +1,74 @@
+(function() {
+  "use strict";
+
+
+  /**
+   * Clica e abre o DropMenu
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * O DropMenu em sí
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+ 
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+
+     /**
+   * Preloader
+   */
+      let preloader = select('#preloader');
+      if (preloader) {
+        window.addEventListener('load', () => {
+          preloader.remove()
+        });
+      } 
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
